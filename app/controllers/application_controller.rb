816806d124009c17460274b437a0f13c5d7deb7a
hiroonrails <m69lover.scachaaan@gmail.com>
2021-02-20 17:34:48 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+#ログインしていないユーザーの時点でログイン画面に移管させる
+before_action :authenticate_user!
+#サインアップ時にnameカラム(名前)をDBに保存させるため、前提として保存できるカラム情報が限られている(deviseの場合)
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    #(:sign_up, keys: [name])＝サインアップ時って意味。
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
